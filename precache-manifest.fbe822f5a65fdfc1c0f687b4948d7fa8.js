86bd7d404a263bc78e0519ae51f64d97cbadcfe7
sebastianRomay <seebasttian.agusttin@gmail.com>
2022-02-09 18:31:32 -0300

    Updates

---
+++ precache-manifest.fbe822f5a65fdfc1c0f687b4948d7fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "698335f3fe0cb1fb4df898d50040914a",
+    "url": "/react-proyecto-ch/index.html"
+  },
+  {
+    "revision": "d17b1407c3d2015ca77e",
+    "url": "/react-proyecto-ch/static/css/main.c4753de9.chunk.css"
+  },
+  {
+    "revision": "33ad4c787b6b46eee083",
+    "url": "/react-proyecto-ch/static/js/2.42bdab1f.chunk.js"
+  },
+  {
+    "revision": "d17b1407c3d2015ca77e",
+    "url": "/react-proyecto-ch/static/js/main.3505f2a0.chunk.js"
+  },
+  {
+    "revision": "2478dd88311b59ed69aa",
+    "url": "/react-proyecto-ch/static/js/runtime~main.177a5b8b.js"
+  },
+  {
+    "revision": "561f38b1f4570de0fb8a39d691ab058c",
+    "url": "/react-proyecto-ch/static/media/Tungsten-Bold.561f38b1.ttf"
+  }
+]);
