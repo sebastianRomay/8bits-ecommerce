19c545235747f3dcc67e2c909f06aa09ad0cbe85
sebastianRomay <seebasttian.agusttin@gmail.com>
2022-02-28 17:39:35 -0300

    Coleccion de clientes creada en firebase

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -3,37 +3,52 @@ import { CartContext } from '../../context/CartContext';
 import swal from 'sweetalert';
 import { Link } from 'react-router-dom';
 import BotonAgregar from '../AgregarCarrito/BotonAgregar';
+import {collection, addDoc} from 'firebase/firestore';
+import {db} from '../../firebase/firebaseConfig';
 
+const estadoInicial = {
+    nombre : '',
+    email : '',
+    telefono : '',
+    localidad : ''
+}
 const Formulario = () => {
 
-    const [nombre, setNombre] = useState()
+    const [valores, setValores] = useState(estadoInicial)
 
-    const obtenerNombre = (e) => {
-        let name = e.target.value
-        setNombre(name)
+    const obtenerValor = (e) => {
+        let {value, name} = e.target;
+        setValores({...valores, [name]: value})
     }
 
     const cartContext = useContext(CartContext);
     const { vaciarCarrito } = cartContext;
 
-    const finCompra = () => {
+    const finCompra = async (e) => {
+        e.preventDefault()
         vaciarCarrito()
-        swal(`Gracias por su compra ${nombre}` , 'En breve sera contactado', 'success')
+        
+        const docRef = await addDoc(collection(db, 'clientes'), {
+            valores
+        })
+        swal('Gracias por su compra', `Su id de transaccion es: ${docRef.id}`, 'success')
+        setValores(estadoInicial)
     }
 
 
   return (
     <>
-        <form className='container mt-5'>
+        <form className='container mt-5' onSubmit={finCompra} >
             <div className="row">
 
         <div className="mb-3 col-md-6 col-12">
             <label for="exampleInputEmail1" className="form-label">Nombre y Apellido</label>
-            <input type="text" className="form-control" required onChange={obtenerNombre}/>
+            <input type="text" className="form-control" name='nombre' value={valores.nombre} required onChange={obtenerValor}/>
+            <div id="emailHelp" className="form-text">Introduzca su nombre completo</div>
         </div>
         <div className="mb-3 col-md-6 col-12">
             <label for="exampleInputEmail1" className="form-label">Email</label>
-            <input type="email" className="form-control" required/>
+            <input type="email" className="form-control" value={valores.email} name='email' onChange={obtenerValor} required/>
             <div id="emailHelp" className="form-text">Introduzca un email correcto</div>
         </div>
 
@@ -42,25 +57,19 @@ const Formulario = () => {
 
             <div className="mb-3 col-md-6 col-12">
             <label for="exampleInputPassword1" className="form-label">Telefono</label>
-            <input type="number" className="form-control" id="exampleInputPassword1" required/>
+            <input type="number" className="form-control" name='telefono' onChange={obtenerValor} value={valores.telefono} id="exampleInputPassword1" required/>
+            <div id="emailHelp" className="form-text">Introduzca su numero telefonico</div>
         </div>
         <div className="mb-3 col-md-6 col-12">
             <label for="exampleInputEmail1" className="form-label">Localidad</label>
-            <input type='text' className="form-control" required/>
+            <input type='text' className="form-control" name='localidad' onChange={obtenerValor} value={valores.localidad} required/>
+            <div id="emailHelp" className="form-text">Introduzca la localidad donde vive</div>
         </div>
 
             </div>
         <div className="row">
-        <div className="mb-3 form-check col-12 d-flex justify-content-center">
-            <input type="checkbox" className="form-check-input mx-1" id="exampleCheck1" required/>
-            <label className="form-check-label" for="exampleCheck1">Acepto terminos y condiciones</label>
-        </div>
-        </div>
-        <div className="row">
             <div className="col-3 mx-auto">
-            <Link to="/">
-                  <BotonAgregar texto='Finalizar Compra' fn={finCompra}/>
-            </Link>
+                  <input type='submit' className='btn btn-outline-dark' value='Finalizar Compra' />
             </div>
         </div>
         </form>
