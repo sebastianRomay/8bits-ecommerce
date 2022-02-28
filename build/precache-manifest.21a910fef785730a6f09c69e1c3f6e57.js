5ed98406851cf4567c7e765bfad74206c68f360e
sebastianRomay <seebasttian.agusttin@gmail.com>
2022-02-28 18:39:16 -0300

    subo carpeta build

---
+++ build/precache-manifest.21a910fef785730a6f09c69e1c3f6e57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbdcb556036047b54dba56240defecda",
+    "url": "/react-proyecto-ch/index.html"
+  },
+  {
+    "revision": "457a6f6d5caf034da701",
+    "url": "/react-proyecto-ch/static/css/main.86b1dd7d.chunk.css"
+  },
+  {
+    "revision": "75a2fd311d03cea9ca8f",
+    "url": "/react-proyecto-ch/static/js/2.e54f154a.chunk.js"
+  },
+  {
+    "revision": "457a6f6d5caf034da701",
+    "url": "/react-proyecto-ch/static/js/main.d9c30d63.chunk.js"
+  },
+  {
+    "revision": "2478dd88311b59ed69aa",
+    "url": "/react-proyecto-ch/static/js/runtime~main.177a5b8b.js"
+  },
+  {
+    "revision": "561f38b1f4570de0fb8a39d691ab058c",
+    "url": "/react-proyecto-ch/static/media/Tungsten-Bold.561f38b1.ttf"
+  }
+]);
--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -3,22 +3,22 @@ import { CartContext } from '../../context/CartContext';
 import swal from 'sweetalert';
 import { Link } from 'react-router-dom';
 import BotonAgregar from '../AgregarCarrito/BotonAgregar';
-import {collection, addDoc} from 'firebase/firestore';
-import {db} from '../../firebase/firebaseConfig';
+import { collection, addDoc } from 'firebase/firestore';
+import { db } from '../../firebase/firebaseConfig';
 
 const estadoInicial = {
-    nombre : '',
-    email : '',
-    telefono : '',
-    localidad : ''
+    nombre: '',
+    email: '',
+    telefono: '',
+    localidad: ''
 }
 const Formulario = () => {
 
     const [valores, setValores] = useState(estadoInicial)
 
     const obtenerValor = (e) => {
-        let {value, name} = e.target;
-        setValores({...valores, [name]: value})
+        let { value, name } = e.target;
+        setValores({ ...valores, [name]: value })
     }
 
     const cartContext = useContext(CartContext);
@@ -27,7 +27,7 @@ const Formulario = () => {
     const finCompra = async (e) => {
         e.preventDefault()
         vaciarCarrito()
-        
+
         const docRef = await addDoc(collection(db, 'clientes'), {
             valores
         })
@@ -36,45 +36,45 @@ const Formulario = () => {
     }
 
 
-  return (
-    <>
-        <form className='container mt-5' onSubmit={finCompra} >
-            <div className="row">
+    return (
+        <>
+            <form className='container mt-5' onSubmit={finCompra} >
+                <div className="row">
 
-        <div className="mb-3 col-md-6 col-12">
-            <label for="exampleInputEmail1" className="form-label">Nombre y Apellido</label>
-            <input type="text" className="form-control" name='nombre' value={valores.nombre} required onChange={obtenerValor}/>
-            <div id="emailHelp" className="form-text">Introduzca su nombre completo</div>
-        </div>
-        <div className="mb-3 col-md-6 col-12">
-            <label for="exampleInputEmail1" className="form-label">Email</label>
-            <input type="email" className="form-control" value={valores.email} name='email' onChange={obtenerValor} required/>
-            <div id="emailHelp" className="form-text">Introduzca un email correcto</div>
-        </div>
+                    <div className="mb-3 col-md-6 col-12">
+                        <label for="exampleInputEmail1" className="form-label">Nombre y Apellido</label>
+                        <input type="text" className="form-control" name='nombre' value={valores.nombre} required onChange={obtenerValor} />
+                        <div id="emailHelp" className="form-text">Introduzca su nombre completo</div>
+                    </div>
+                    <div className="mb-3 col-md-6 col-12">
+                        <label for="exampleInputEmail1" className="form-label">Email</label>
+                        <input type="email" className="form-control" value={valores.email} name='email' onChange={obtenerValor} required />
+                        <div id="emailHelp" className="form-text">Introduzca un email correcto</div>
+                    </div>
 
-            </div>
-            <div className="row">
+                </div>
+                <div className="row">
 
-            <div className="mb-3 col-md-6 col-12">
-            <label for="exampleInputPassword1" className="form-label">Telefono</label>
-            <input type="number" className="form-control" name='telefono' onChange={obtenerValor} value={valores.telefono} id="exampleInputPassword1" required/>
-            <div id="emailHelp" className="form-text">Introduzca su numero telefonico</div>
-        </div>
-        <div className="mb-3 col-md-6 col-12">
-            <label for="exampleInputEmail1" className="form-label">Localidad</label>
-            <input type='text' className="form-control" name='localidad' onChange={obtenerValor} value={valores.localidad} required/>
-            <div id="emailHelp" className="form-text">Introduzca la localidad donde vive</div>
-        </div>
+                    <div className="mb-3 col-md-6 col-12">
+                        <label for="exampleInputPassword1" className="form-label">Telefono</label>
+                        <input type="number" className="form-control" name='telefono' onChange={obtenerValor} value={valores.telefono} id="exampleInputPassword1" required />
+                        <div id="emailHelp" className="form-text">Introduzca su numero telefonico</div>
+                    </div>
+                    <div className="mb-3 col-md-6 col-12">
+                        <label for="exampleInputEmail1" className="form-label">Localidad</label>
+                        <input type='text' className="form-control" name='localidad' onChange={obtenerValor} value={valores.localidad} required />
+                        <div id="emailHelp" className="form-text">Introduzca la localidad donde vive</div>
+                    </div>
 
-            </div>
-        <div className="row">
-            <div className="col-3 mx-auto">
-                  <input type='submit' className='btn btn-outline-dark' value='Finalizar Compra' />
-            </div>
-        </div>
-        </form>
-    </>
-  )
+                </div>
+                <div className="row">
+                    <div className="col-3 mx-auto">
+                        <input type='submit' className='btn btn-outline-dark' value='Finalizar Compra' />
+                    </div>
+                </div>
+            </form>
+        </>
+    )
 }
 
 export default Formulario
